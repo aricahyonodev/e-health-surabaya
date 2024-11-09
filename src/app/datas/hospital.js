50b0d77829a7aa5f8d801b0bd48f4e6e42d54dba
aricahyonodev <cahyono.ari81@gmail.com>
2024-11-09 09:33:20 +0700

    add : hospital policlinic page

---
+++ src/app/datas/hospital.js
@@ -0,0 +1,14 @@
+export const listHospital = [
+  {
+    hospitalId: 1,
+    name: "RSUD  Dr. Mohammad Soewandhie",
+    address: "Jl. Tambak Rejo No.45-47, Tambakrejo Simokerto Surabaya",
+    image: "rsud-dr-soewandhie.png",
+  },
+  {
+    hospitalId: 2,
+    name: "RSUD Bhakti Darma Husada",
+    address: "Jl. Raya Kendung 115 - 117, Sememi Benowo Surabaya",
+    image: "rsud-bdh.png",
+  },
+];
